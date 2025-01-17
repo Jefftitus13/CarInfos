9b49770a0f587b6246acdcb94a94894ad7d311a7
JAFRY <JAFRY@LAPTOP-OPA8AQMV>
2025-01-17 16:53:33 +0530

    CSV and Excel get methods

--- CarInfos/Controllers/CarsController.cs
+++ CarInfos/Controllers/CarsController.cs
@@ -1,10 +1,11 @@
 ﻿using CarInfos.Data;
 using CarInfos.Models.Entity;
 using CarInfos.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
+using System.Text;
+using OfficeOpenXml;
 
 namespace CarInfo.Controllers
 {
@@ -38,6 +39,78 @@ namespace CarInfo.Controllers
             }
             return Ok(car);
         }
+
+        [HttpGet]
+        [Route("export")]
+        [Authorize]
+        public IActionResult ExportCars([FromQuery] string format = "csv, xlsx")
+        {
+            var cars = dbContext.Cars.ToList();
+
+            if (!cars.Any())
+            {
+                return NotFound("There's no car data available to export!");
+            }
+
+            switch (format.ToLower())
+            {
+                case "csv":
+                    var csvContent = GenerateCsv(cars);
+                    return File(System.Text.Encoding.UTF8.GetBytes(csvContent), "text/csv", "CarsExport.csv");
+
+                case "excel":
+                    var excelContent = GenerateExcel(cars);
+                    return File(excelContent, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "CarsExport.xlsx");
+
+                default:
+                    return BadRequest("Invalid format");
+            }
+        }
+        private string GenerateCsv(IEnumerable<Cars> cars)
+        {
+            var csvBuilder = new StringBuilder();
+            csvBuilder.AppendLine("Id,Brand,Model,Year,Color,Price,Description,Mileage,IsAvailable");
+            
+            foreach (var car in cars)
+            {
+                csvBuilder.AppendLine($"{car.Id},{car.Brand},{car.Model},{car.Year},{car.Color},{car.Price},{car.Description},{car.Mileage},{car.IsAvailable}");
+            }
+            return csvBuilder.ToString();
+        }
+
+        private byte[] GenerateExcel(IEnumerable<Cars> cars)
+        {
+            using(var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Cars");
+                worksheet.Cells[1, 1].Value = "Id";
+                worksheet.Cells[1, 2].Value = "Brand";
+                worksheet.Cells[1, 3].Value = "Model";
+                worksheet.Cells[1, 4].Value = "Year";
+                worksheet.Cells[1, 5].Value = "Color";
+                worksheet.Cells[1, 6].Value = "Price";
+                worksheet.Cells[1, 7].Value = "Description";
+                worksheet.Cells[1, 8].Value = "Mileage";
+                worksheet.Cells[1, 9].Value = "IsAvailable";
+
+                var row = 2;
+                foreach (var car in cars)
+                {
+                    worksheet.Cells[row, 1].Value = car.Id;
+                    worksheet.Cells[row, 2].Value = car.Brand;
+                    worksheet.Cells[row, 3].Value = car.Model;
+                    worksheet.Cells[row, 4].Value = car.Year;
+                    worksheet.Cells[row, 5].Value = car.Color;
+                    worksheet.Cells[row, 6].Value = car.Price;
+                    worksheet.Cells[row, 7].Value = car.Description;
+                    worksheet.Cells[row, 8].Value = car.Mileage;
+                    worksheet.Cells[row, 9].Value = car.IsAvailable;
+                    row++;
+                }
+                return package.GetAsByteArray();
+            }
+        }
+    
 
         //Post method
         [HttpPost]
